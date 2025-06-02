20fc80ee1c5f0c909f5b919ce8aedd237a2eaefc
Alex <pan19772012@gmail.com>
2025-06-03 00:03:45 +0400

    new_commit

--- tests.py
+++ tests.py
@@ -1,24 +1,51 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    @pytest.fixture
+    def collector(self):
+        """Создает новый экземпляр BooksCollector для каждого теста."""
+        return BooksCollector()
 
-        # добавляем две книги
+
+    def test_add_new_book_add_two_books(self, collector):
+
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
+
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+
+    def test_add_new_book_over_limit(self, collector):
+        """Тест на добавление книги с длинным названием (больше 40 символов)"""
+        long_name = 'К' * 41
+        collector.add_new_book(long_name)
+        assert len(collector.get_books_rating()) == 0
+
+    def test_readd_book(self, collector):
+        """Тест на повторное добавление одной и той же книги"""
+        book_name = '1984'
+        collector.add_new_book(book_name)
+        collector.add_new_book(book_name)
+
+        assert len(collector.get_books_rating()) == 1
+
+    def test_set_book_genre(self, collector):
+        """Тест на установку жанра книги"""
+        book_name = 'Моби Дик'
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, 'Приключения')
+
+        assert collector.get_book_genre(book_name) == 'Приключения'
+
+    def test_books_for_children(self, collector):
+        """Тест на получение списка книг, подходящих для детей"""
+        collector.add_new_book('Кот в шляпе')
+        collector.set_book_genre('Кот в шляпе', 'Детская литература')
+
+        assert 'Кот в шляпе' in collector.get_books_for_children()  
